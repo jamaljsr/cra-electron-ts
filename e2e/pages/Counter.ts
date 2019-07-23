1ce3ea49f3b4c68fa4133c2ebd81e3d963c03297
jamaljsr <1356600+jamaljsr@users.noreply.github.com>
2019-07-23 10:44:33 -0400

    test(e2e): update e2e tests (#14)

--- e2e/pages/Counter.ts
+++ e2e/pages/Counter.ts
@@ -7,8 +7,10 @@ class Counter {
   incrementOdd: Selector = Selector('[data-tid=odd-btn]');
   incrementAsync: Selector = Selector('[data-tid=async-btn]');
   loadingIcon: Selector = Selector('[data-tid=async-loader]');
+  error: Selector = Selector('[data-tid=error]');
 
   getCounterText = () => this.counter.innerText;
+  getErrorText = () => this.error.innerText;
 }
 
 export default new Counter();
