dccf84a5f18c0febae9ff27fd99f3b8a7aaeff5e
jamaljsr <jamal@farrock.com>
2019-07-23 11:24:18 -0400

    feat(home): add lorem text to the home page

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -16,15 +16,23 @@ const Home = () => {
         <Alert message="Success Tips" type="success" showIcon data-tid="success" />
       )}
       <Card title="Welcome to React App">
-        <p>Let&apos;s get you started...</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+          nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+          fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
         <p>
           Play with the{' '}
           <Link to={COUNTER} data-tid="counter-link">
             Counter
-          </Link>
+          </Link>{' '}
+          thing
         </p>
         <p>
-          <Button data-tid="me-btn" onClick={handleClickMe}>
+          <Button type="primary" data-tid="me-btn" onClick={handleClickMe}>
             Click Me!
           </Button>
         </p>
