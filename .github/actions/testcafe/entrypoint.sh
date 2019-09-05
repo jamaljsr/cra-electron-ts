c8a5ee875b997ba4495991cadcfc0371c2d6708f
jamaljsr <jamal@farrock.com>
2019-09-05 00:57:40 -0400

    ci(github): install yarn in the testcafe action container

--- .github/actions/testcafe/entrypoint.sh
+++ .github/actions/testcafe/entrypoint.sh
@@ -8,13 +8,13 @@ Xvfb -ac :99 -screen 0 1280x1024x16 > /dev/null 2>&1 &
 export DISPLAY=:99.0
 
 # Run commands
-cmd=$@
-echo "Running '$cmd'..."
-if "$cmd"; then
+cmd=$*
+echo "Running 'yarn $cmd'..."
+if yarn "$cmd"; then
   # no op
-  echo "Successfully ran '$cmd'"
+  echo "Successfully ran 'yarn $cmd'"
 else
   exit_code=$?
-  echo "Failure running '$cmd', exited with $exit_code"
+  echo "Failure running 'yarn $cmd', exited with $exit_code"
   exit $exit_code
 fi
